36634efb44109ac82811cbf384fc12b5be503f05
yusseno <yusseno19@gmail.com>
2023-01-14 10:02:37 +0700

    add link project github

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -34,13 +34,7 @@ const Projects = () => {
                 <div
                     class="flex justify-center items-center opacity-0 bg-gradient-to-t from-gray-800 via-gray-800 to-opacity-30 group-hover:opacity-50 absolute top-0 left-0 h-full w-full">
                 </div>
-                <div class="absolute top-0 left-0 w-full h-full flex justify-center items-center opacity-0 hover:opacity-100">
-                    <div class="flex-row text-center">
-                    <h1 class="text-gray-50 font-bold text-lg">Be careful on the way.</h1>
-                    <p class="text-gray-200 font-medium text-sm">Tulus</p>
-                    <small class="text-xs font-light text-gray-300">Photo by Sébastien Goldberg on Unsplash</small>
-                    </div>
-                </div>
+               
             </div>
             <div className="grid grid-cols-2 pt-8">
                 <div className="">
